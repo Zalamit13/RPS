17504e33247c84d20f1ddf4bffac8ffabd731ffa
Zalamit13 <115380255+Zalamit13@users.noreply.github.com>
2023-10-05 11:26:32 +0100

    that tooks a goddamn while
    all player options in the playround func

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function getComputerChoice (){
     let rand = '';
     let msg;
     rand = Math.floor(Math.random() * 3) + 1;
-    if (rand == 1) msg = "Rock";
-    else if (rand == 2) msg = "Paper";
-    else  msg = "Scissors"
+    if (rand == 1) msg = "rock";
+    else if (rand == 2) msg = "paper";
+    else  msg = "scissors"
     return msg;
 }
 
@@ -14,8 +14,21 @@ function playerSelection() {
     if (choice === "rock" || choice === "paper" || choice === "scissors") {
         return choice;
     } else {
-        console.log("Invalid choice. Please choose Rock, Paper, or Scissors.");
-        return null;
+        console.log("Invalid choice. Please choose Rock, Paper, or Scissors. Reload the page");
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    let rslt;
+    if (playerSelection == "rock" && computerSelection == "scissors") rslt = "You win! Rock beats scissors";
+    else if (playerSelection == "rock" && computerSelection == "paper") rslt = "You lose! Paper beats rock";
+    else if (playerSelection == "rock" && computerSelection == "rock") rslt = "Draw! You both chose the same thing";
+    else if (playerSelection == "paper" && computerSelection == "scissors") rslt = "You lose! Scissors beats paper";
+    else if (playerSelection == "paper" && computerSelection == "paper") rslt = "Draw! You both chose the same thing";
+    else if (playerSelection == "paper" && computerSelection == "rock") rslt = "You Win! Paper beats rock";
+    else if (playerSelection == "scissors" && computerSelection == "scissors") rslt = "Draw! You both chose the same thing";
+    else if (playerSelection == "scissors" && computerSelection == "paper") rslt = "You Win! Scissors beats paper";
+    else if (playerSelection == "scissors" && computerSelection == "rock") rslt = "You lose! Rock beats scissors";
+    
+    return rslt; 
+  }
